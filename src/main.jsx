866604b1ab77a71c842e623b00b59c9ad30ee140
Vijay <vijayotaradi11@gmail.com>
2024-12-23 09:13:17 +0530

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./App.css";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    hiii vijay
+      <nav className="navbar">
+      <div className="logo">MyWebsite</div>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+    <h1>Hello, Vijay!</h1>
+
   </StrictMode>,
 )
